5ab348077da1d5c29bb7a520525da026430a20c1
Aivaras Gotovskis <aivaras.gotovskis@nfq.lt>
2018-04-30 18:21:16 +0300

    Set correct permissions.

--- tools/docker/develop_init.sh
+++ tools/docker/develop_init.sh
@@ -8,4 +8,13 @@ PROJECT_ROOT="$(dirname $(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))
 echo "PROJECT ROOT: ${PROJECT_ROOT}"
 cd "${PROJECT_ROOT}"
 
+function setPerms {
+	mkdir -p $1
+	sudo setfacl -R  -m m:rwx -m u:33:rwX -m u:1000:rwX $1
+	sudo setfacl -dR -m m:rwx -m u:33:rwX -m u:1000:rwX $1
+}
+
+echo -e '\n## Setting up permissions ... '
+setPerms "${PROJECT_ROOT}/var"
+
 ./install.sh
